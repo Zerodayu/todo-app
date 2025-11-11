1403a5998e4c016c0bfd08284aa58efb39160ff1
Zerodayu <kimianalconera39@gmail.com>
2025-11-12 02:37:46 +0800

    update: todos schema

--- backend/db/schema.ts
+++ backend/db/schema.ts
@@ -1,12 +1,25 @@
-import { uuid, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { uuid, pgTable, text, timestamp, serial, integer, boolean } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users', {
-  id: uuid('id').primaryKey().defaultRandom(),
+  id: serial('id').primaryKey(),
   name: text('name'),
   email: text('email').notNull().unique(),
   password: text('password').notNull(),
   created_at: timestamp('created_at').notNull().defaultNow(),
 });
 
+export const todosTable = pgTable('todos', {
+  id: serial('id').primaryKey(),
+  user_id: integer('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  title: text('title').notNull(),
+  is_done: boolean('is_done').notNull().default(false),
+  created_at: timestamp('created_at').notNull().defaultNow(),
+});
+
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
+
+export type InsertTodo = typeof todosTable.$inferInsert;
+export type SelectTodo = typeof todosTable.$inferSelect;
