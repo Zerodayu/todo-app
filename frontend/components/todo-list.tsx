4a54930b8734118eae510947f147cb4ee9a172b3
Zerodayu <kimianalconera39@gmail.com>
2025-11-12 16:24:48 +0800

    update: dynamic list

--- frontend/components/todo-list.tsx
+++ frontend/components/todo-list.tsx
@@ -1,5 +1,9 @@
 import { Button } from "./ui/button"
 import { Badge } from "./ui/badge"
+import {
+  IsLoggedIn,
+  IsLoggedOut,
+} from "./useSession"
 
 const todos = [
   { id: 1, title: "First Todo", isDone: true },
@@ -9,17 +13,24 @@ const todos = [
 export default function ShowTodos() {
   return (
     <section className="flex w-xl flex-col gap-4">
-      {todos.map((todo) => (
-        <div key={todo.id} className="flex items-center justify-between p-4 border rounded-lg">
-          <span className="flex flex-col">
-            <Badge {...(todo.isDone ? {} : { variant: "outline" })}>
-              {todo.isDone ? "Done" : "Pending"}
-            </Badge>
-            <p>{todo.title}</p>
-          </span>
-          <Button>{todo.isDone ? "Mark as Undone" : "Mark as Done"}</Button>
-        </div>
-      ))}
+      <IsLoggedOut>
+        <p className="text-center text-muted-foreground">
+          Please login to see your todos.
+        </p>
+      </IsLoggedOut>
+      <IsLoggedIn>
+        {todos.map((todo) => (
+          <div key={todo.id} className="flex items-center justify-between p-4 border rounded-lg">
+            <span className="flex flex-col">
+              <Badge {...(todo.isDone ? {} : { variant: "outline" })}>
+                {todo.isDone ? "Done" : "Pending"}
+              </Badge>
+              <p>{todo.title}</p>
+            </span>
+            <Button>{todo.isDone ? "Mark as Undone" : "Mark as Done"}</Button>
+          </div>
+        ))}
+      </IsLoggedIn>
     </section>
   )
 }
